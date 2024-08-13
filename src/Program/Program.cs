0b668519e07463d94693db27a3c9cedf0a4ab596
unknown <GastonPuyares>
2024-08-13 10:29:01 -0300

    Implementación de la operación <addition>

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
