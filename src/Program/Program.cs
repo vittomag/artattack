db7575d3d6a90c9745b0990d5c38443f225e8db3
unknown <candelamangino>
2024-08-13 10:38:02 -0300

    resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clase implementa la operación resta
+public class Resta
+{
+    public static int Restar(int a, int b)
+    {
+        return a - b;
+    }
+}
